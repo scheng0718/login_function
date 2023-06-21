ed4b6bea98d6669f4b32817f17f9d66a9fbcda39
scheng0718 <scheng0718@gmail.com>
2023-06-21 00:24:14 -0700

    refactor: add config folder and modify mongoose settings

--- app.js
+++ app.js
@@ -1,25 +1,10 @@
 const express = require('express')
-const mongoose = require('mongoose')
+
 const exphbs = require('express-handlebars')
 const routes = require('./routes')
 const app = express()
 const port = 3000
-
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true 
-})
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+require('./config/mongoose')
 app.engine('hbs', exphbs({
   defaultLayout: 'main',
   extname: 'hbs'
