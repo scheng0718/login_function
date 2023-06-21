8e02d6d5cf15434a040ac4e9826180e8a3bdb19d
scheng0718 <scheng0718@gmail.com>
2023-06-21 14:15:26 -0700

    feat: set up login and register route and page

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,10 @@
 // 引用 Express 與 Express 路由器
 const express = require('express')
 const router = express.Router()
-// 引入 home 模組程式碼
-const home = require('./modules/home')
-// 引入 welcome 模組程式碼
-const welcome = require('./modules/welcome')
+// 引入 user 模組程式碼
+const users = require('./modules/users')
 // 將網址結構符合 / 字串的 request 導向 home 模組 
-router.use('/', home)
-router.use('/login', welcome)
+router.use('/users', users)
+
 // 匯出路由器
 module.exports = router
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,15 +3,25 @@ const express = require('express')
 const router = express.Router()
 // 引用 Login model
 const Login = require('../../models/login')
+// 定義登入路由
+router.get('/login', (req, res) => {
+  const error = req.query.error === 'true';
+  res.render('login', { error })
+})
+// 定義註冊路由
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
 // 定義登入功能路由
-router.post('/', (req, res) => {
+router.post('/login', (req, res) => {
   const email = req.body.email
   const password = req.body.password
   Login.findOne({email, password})
     .lean()
     .then(user => {
       if (!user) {
-        res.redirect('/?error=true')
+        res.redirect('/users/login/?error=true')
       } else {
         const firstName = user.firstName
         res.render('welcome', { firstName })
