1d7cc09d49aac4798e0d2ddb6810f00be3a5b1af
scheng0718 <scheng0718@gmail.com>
2023-06-20 18:38:59 -0700

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
@@ -18,9 +19,14 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb connected!')
 })
+app.engine('hbs', exphbs({
+  defaultLayout: 'main',
+  extname: 'hbs'
+}))
+app.set('view engine', 'hbs')
 
 app.use('/', (req, res) => {
-  res.send('This is first page')
+  res.render('index')
 })
 
 app.listen(port, () => {
