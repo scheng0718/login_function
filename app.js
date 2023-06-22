112e925cb437bba59ce91503eda359214ae04095
scheng0718 <scheng0718@gmail.com>
2023-06-21 17:55:19 -0700

    feat: remove cookie-parser

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
-const cookieParser = require('cookie-parser')
+// const cookieParser = require('cookie-parser')
 const routes = require('./routes')
 const app = express()
 const port = 3000
@@ -13,7 +13,7 @@ app.engine('hbs', exphbs({
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser())
+// app.use(cookieParser())
 app.use(session({
   secret: 'ThisIsMySecret',
   resave: true,
